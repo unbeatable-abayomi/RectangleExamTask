01c7d4de60fe73239bd2ba401125a94a7ede660a
abayomi <53374703+unbeatable-abayomi@users.noreply.github.com>
2020-04-17 15:43:09 +0100

    Add project files.

---
+++ RectangleExamTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RectangleExamTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(3,5);
+
+            Console.WriteLine($"{rectangle.ToString()}");
+        }
+    }
+}
---
+++ RectangleExamTask/Rectangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleExamTask
+{
+    class Rectangle
+    {
+        private double length = 1;
+        private double width = 1;
+
+
+
+        public Rectangle(double lngt, double widt)
+        {
+            this.length = lngt;
+            this.width = widt;
+
+        }
+
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                if (value > 0.0 && value < 20.0)
+                {
+                    length = value;
+                }
+
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value > 0.0 && value < 20.0)
+                {
+                    width = value;
+                }
+
+            }
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                return (Length + Width) * 2;
+            }
+        }
+
+        public double AreaOfRectangle
+        {
+            get
+            {
+                return (Length * Width);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Length :{Length} Width: {Width} Area {AreaOfRectangle}";
+        }
+    }
+}
